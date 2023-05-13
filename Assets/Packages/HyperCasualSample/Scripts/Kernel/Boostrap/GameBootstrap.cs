0d3eec2c2b6112b9fa63d93a4e429282ec6b7352
Oleksandr Tokar <oleksandr.tokar@bini.games>
2023-05-14 01:18:17 +0300

    Make movements

--- Assets/Packages/HyperCasualSample/Scripts/Kernel/Boostrap/GameBootstrap.cs
+++ Assets/Packages/HyperCasualSample/Scripts/Kernel/Boostrap/GameBootstrap.cs
@@ -6,15 +6,31 @@ public class GameBootstrap : MonoBehaviour
 {
    private SceneLoader _sceneLoader;
    private string _gameplaySceneName;
+   private InputService _inputService;
    
    private void Awake()
    {
       ServiceLocator.Instance.RegisterSingle(new SceneLoader());
+      ServiceLocator.Instance.RegisterSingle(new InputService());
       _sceneLoader = ServiceLocator.Instance.Single<SceneLoader>();
+      _inputService = ServiceLocator.Instance.Single<InputService>();
    }
 
    private void Start()
    {
-      _sceneLoader.LoadWith(_gameplaySceneName);
-   } 
+      
+      //_sceneLoader.LoadWith(_gameplaySceneName);
+   }
+
+   private void Update()
+   {
+      Debug.Log(_inputService.Axis);
+   }
+}
+
+public class InputService : IService
+{
+   private const string Horizontal = "Horizontal";
+   private const string Vertical = "Vertical";
+   public Vector2 Axis => new(SimpleInput.GetAxis(Horizontal), SimpleInput.GetAxis(Vertical));
 }
---
+++ Assets/Packages/HyperCasualSample/Scripts/Kernel/HeroMove.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace Packages.HyperCasualSample.Scripts.Kernel
+{
+    public class HeroMove : MonoBehaviour
+    {
+        public CharacterController CharacterController;
+        public float MovementSpeed;
+
+        private InputService _inputService;
+        private Camera _camera;
+
+
+        private void Start()
+        {
+            _inputService = ServiceLocator.Instance.Single<InputService>();
+            _camera = Camera.main;
+        } 
+
+        private void Update()
+        {
+            Vector3 movementVector = Vector3.zero;
+
+            if (_inputService.Axis.sqrMagnitude > 0.001f)
+            {
+                movementVector = _camera.transform.TransformDirection(_inputService.Axis);
+                movementVector.y = 0;
+                movementVector.Normalize();
+
+                transform.forward = movementVector;
+            }
+
+           // movementVector += Physics.gravity;
+
+            CharacterController.Move(MovementSpeed * movementVector * Time.deltaTime);
+        }
+    }
+}
